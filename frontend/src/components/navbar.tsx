8aa85bb3b1972ac0c9ce1a1ea6610ba0378fe196
Flaryiest <ericmzuo1@gmail.com>
2025-01-22 18:05:57 -0700

    removed unwanted styling and html

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -3,62 +3,8 @@ import { Link } from 'react-router-dom'
 import '../style/navbar.css'
 
 export default function Navbar() {
-    const [menuOpen, setMenuOpen] = useState(false)
+    return <div className="navbar-container">
+        <div className="navbar"></div>
 
-    const toggleMenu = () => {
-        setMenuOpen((prev) => !prev)
-    }
-
-    return (
-        <div className="navbar-container">
-            <header className="header">
-                <Link to="/" className="logo">
-                    Test
-                </Link>
-                <nav className="nav">
-                    <ul className="nav-links">
-                        <Link to="/info" className="nav-link">
-                            <li>Info</li>
-                        </Link>
-                        <Link to="/login" className="nav-link">
-                            <li>Sign In</li>
-                        </Link>
-                        <Link to="/signup" className="nav-link sign-up-button">
-                            <li>Sign Up</li>
-                        </Link>
-                        <button className="menu" onClick={toggleMenu}>
-                            <img src="assets/menu.svg" alt="Menu" />
-                        </button>
-                    </ul>
-                </nav>
-            </header>
-            {menuOpen && (
-                <div className="fullscreen-menu">
-                    <ul className="fullscreen-links">
-                        <li>
-                            <Link to="/" onClick={toggleMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/info" onClick={toggleMenu}>
-                                Info
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/login" onClick={toggleMenu}>
-                                Login
-                            </Link>
-                        </li>
-
-                        <li>
-                            <Link to="/signup" onClick={toggleMenu}>
-                                Sign Up
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            )}
-        </div>
-    )
+    </div>
 }
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -10,8 +10,6 @@ import DashboardLayout from './layouts/dashboardLayout.tsx'
 
 import IndexPage from './pages/index.tsx'
 import InfoPage from './pages/info.tsx'
-import LoginPage from './pages/login.tsx'
-import SignUpPage from './pages/signUp.tsx'
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -21,8 +19,6 @@ const router = createBrowserRouter(
                 <Route path="/info" element={<InfoPage />} />
             </Route>
             <Route element={<OnboardLayout />}>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/signup" element={<SignUpPage />} />
             </Route>
             <Route element={<DashboardLayout />}></Route>
         </>
