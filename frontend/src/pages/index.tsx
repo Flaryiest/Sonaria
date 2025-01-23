070a46154f49aa8fb8b1a798d06cb94f2161142e
Flaryiest <ericmzuo1@gmail.com>
2025-01-22 20:32:38 -0700

    created grid for index section one

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,7 +5,11 @@ import Fade from 'react-awesome-reveal'
 export default function IndexPage() {
     return <div className="index-page-container">
         <div className="index-page">
-
+            <div className="index-page-section-one">
+                <div className="grid">
+                    <h1 className="index-page-section-one-header">Where Music Meets Magic</h1>
+                </div>
+            </div>
         </div>
     </div>
 }
