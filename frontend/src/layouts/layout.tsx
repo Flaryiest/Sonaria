cc8015b0830321598e71500db3f437aadda156a1
Flaryiest <ericmzuo1@gmail.com>
2025-01-26 09:50:19 -0700

    created scroll to top component

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/navbar.jsx'
 import AnimatedCursor from 'react-animated-cursor'
+import ScrollToTop from '../components/scrollToTop.js'
 export default function Layout() {
     return (
         <>
             <Navbar />
             <Outlet />
+            <ScrollToTop/>
             <div className="cursor-wrapper">
                 <AnimatedCursor color="0,0,0" />
             </div>
